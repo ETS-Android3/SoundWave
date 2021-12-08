c94bc39c3c64757414a2606457328d8351daccdd
nikos <nikoszannakis14@gmail.com>
2021-12-08 04:47:36 +0200

    Profile Menu 2

---
+++ app/src/main/java/com/example/projectlogin/ui/login/ListAdapter.java
@@ -0,0 +1,54 @@
+package com.example.projectlogin.ui.login;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.projectlogin.R;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends ArrayAdapter<Song> {
+
+
+    public ListAdapter(Context context, ArrayList<Song> userArrayList){
+
+        super(context,R.layout.list_adapter_song,userArrayList);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        Song song = getItem(position);
+
+        if (convertView == null){
+
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_adapter_song,parent,false);
+
+        }
+
+        ImageView imageView = convertView.findViewById(R.id.profile_pic);
+        TextView userName = convertView.findViewById(R.id.personName);
+        TextView lastMsg = convertView.findViewById(R.id.lastMessage);
+
+//        imageView.;
+        userName.setText(song.name);
+        lastMsg.setText(song.lastMessage);
+        Picasso.get().load(song.imageId).placeholder(R.drawable.missingbackground).error(R.drawable.missingbackground).fit().centerCrop().into(imageView);
+
+
+        return convertView;
+    }
+}
