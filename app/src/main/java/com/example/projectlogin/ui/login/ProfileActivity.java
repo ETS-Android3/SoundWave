c0e1b9f2f4827ab02098fae8f35d1e3cd9c027b5
nikos <nikoszannakis14@gmail.com>
2022-01-19 03:32:45 +0200

    Add Button Settings in Profile page

--- app/src/main/java/com/example/projectlogin/ui/login/ProfileActivity.java
+++ app/src/main/java/com/example/projectlogin/ui/login/ProfileActivity.java
@@ -2,72 +2,172 @@ package com.example.projectlogin.ui.login;
 
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.projectlogin.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.UUID;
 
 
+public class ProfileActivity extends AppCompatActivity {
 
-
-import com.example.projectlogin.R;
-
-
-public class ProfileActivity  extends AppCompatActivity {
-
-    ImageButton btnBackProfile;
-    Button btnAvatar;
-    ImageButton ImageAvatar;
+    ImageView ImageAvatar;
+    TextView emailText;
+    TextView phoneText;
+    TextView userNameText;
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
+    String userEmail;
+    ProgressDialog progressDialog;
     int SELECT_PICTURE = 200;
+    private Button btnSelect, btnUpload;
+    private Uri filePath;
+    private final int PICK_IMAGE_REQUEST = 22;
+    Button btnsettings;
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
 
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_menu);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            userEmail = user.getEmail();
+        }
+        db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("users").document(userEmail);
+        //String urlImage = "images/" + currentuser + "/profile.jpg";
 
 
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                progressDialog = new ProgressDialog(ProfileActivity.this);
+                progressDialog.setMessage("Loading");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("DOCUMENTDATA", "DocumentSnapshot data: " + document.getData());
+                        emailText.setText(document.getString("email"));
+                        phoneText.setText(document.getString("phoneNumber"));
+                        userNameText.setText(document.getString("username"));
+                        progressDialog.dismiss();
+                    } else {
+                        Log.d("DOCUMENTMISSING", "No such document");
+                        progressDialog.dismiss();
+                    }
+                } else {
+                    Log.d("DOCUMENTERROR", "get failed with ", task.getException());
+                    progressDialog.dismiss();
+                }
+            }
+        });
+
+        emailText = findViewById(R.id.Email);
+        phoneText = findViewById(R.id.Phone);
+        userNameText = findViewById(R.id.Username);
+        setupBottomNavigation();
+
+        // initialise views
+        btnSelect = findViewById(R.id.btnChoose);
+        btnUpload = findViewById(R.id.btnUpload);
 
 
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
-
-
-
-
-        ImageAvatar = findViewById(R.id. ImageAvatar);
-        ImageAvatar = findViewById(R.id.ImageAvatar);
-
-
-        ImageAvatar.setOnClickListener(new View.OnClickListener() {
+        // on pressing btnSelect SelectImage() is called
+        btnSelect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageChooser();
+                SelectImage();
+            }
+        });
+
+        // on pressing btnUpload uploadImage() is called
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
+        findViewById(R.id.btn_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, SettingsActivity.class));
+            }
+        });
+
+        String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference urlImage = storageRef.child("images/" + currentuser + "/profile.jpg" );
+        ImageAvatar = findViewById(R.id.ImageAvatar);
+        Picasso.get().load(urlImage.toString()).into(ImageAvatar);
+        storageRef.child("images/" + currentuser + "/profile.jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                ImageAvatar = findViewById(R.id.ImageAvatar);
+                Picasso.get().load(uri).into(ImageAvatar);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
             }
         });
     }
 
 
-    void imageChooser() {
+    private void SelectImage() {
 
-
-        Intent i = new Intent();
-        i.setType("image/*");
-        i.setAction(Intent.ACTION_GET_CONTENT);
-
-        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+        // Defining Implicit Intent to mobile gallery
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."),
+                PICK_IMAGE_REQUEST);
     }
 
 
@@ -75,17 +175,115 @@ public class ProfileActivity  extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == RESULT_OK) {
-
-
             if (requestCode == SELECT_PICTURE) {
-
                 Uri selectedImageUri = data.getData();
                 if (null != selectedImageUri) {
-
                     ImageAvatar.setImageURI(selectedImageUri);
                 }
             }
         }
+        if (requestCode == PICK_IMAGE_REQUEST
+                && resultCode == RESULT_OK
+                && data != null
+                && data.getData() != null) {
+
+            // Get the Uri of data
+            filePath = data.getData();
+            try {
+
+                // Setting image on image view using Bitmap
+                Bitmap bitmap = MediaStore
+                        .Images
+                        .Media
+                        .getBitmap(
+                                getContentResolver(),
+                                filePath);
+                ImageAvatar.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                // Log the exception
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImage() {
+        if (filePath != null) {
+
+            // Code for showing progressDialog while uploading
+            ProgressDialog progressDialog
+                    = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+            String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+            // Defining the child of storageReference
+            StorageReference ref
+                    = storageReference
+                    .child(
+                            "images/"
+                                    + currentuser + "/profile.jpg" );
+            // adding listeners on upload
+            // or failure of image
+            ref.putFile(filePath)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                                @Override
+                                public void onSuccess(
+                                        UploadTask.TaskSnapshot taskSnapshot) {
+
+                                    // Image uploaded successfully
+                                    // Dismiss dialog
+                                    progressDialog.dismiss();
+                                    Toast
+                                            .makeText(ProfileActivity.this,
+                                                    "Image Uploaded!!",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                }
+                            })
+
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                            // Error, Image not uploaded
+                            progressDialog.dismiss();
+                            Toast
+                                    .makeText(ProfileActivity.this,
+                                            "Failed " + e.getMessage(),
+                                            Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    })
+                    .addOnProgressListener(
+                            new OnProgressListener<UploadTask.TaskSnapshot>() {
+
+                                // Progress Listener for loading
+                                // percentage on the dialog box
+                                @Override
+                                public void onProgress(
+                                        UploadTask.TaskSnapshot taskSnapshot) {
+                                    double progress
+                                            = (100.0
+                                            * taskSnapshot.getBytesTransferred()
+                                            / taskSnapshot.getTotalByteCount());
+                                    progressDialog.setMessage(
+                                            "Uploaded "
+                                                    + (int) progress + "%");
+                                }
+                            });
+        }
+    }
+
+    private void setupBottomNavigation(){
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationHelper.enableNavigation(ProfileActivity.this,bottomNavigationViewEx);
+
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(2);
+        menuItem.setChecked(true);
     }
 }
 
